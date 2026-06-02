0a3d2beefe85752a0e4b1879188b10ff95da894e
Sayomikun Ayeni <sayomikun.ayeni@gmail.com>
2026-06-02 11:25:43 -0400

    Gradient Descent .h file with functions add

---
+++ include/GradientDescent.h
@@ -0,0 +1,16 @@
+#ifndef GRADIENTDESCENT_H
+#define GRADIENTDESCENT_H
+
+
+#include "../include/NumericalMethod.h"
+#include "../include/FunctionEvaluator.h"
+
+
+class GradientDescent : public NumericalMethod {
+private:
+
+public:
+
+};
+
+#endif // GRADIENTDESCENT_H
